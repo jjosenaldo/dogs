ac84845bb68a0449e6d95b1e87ccdb3d98872cbe
= <=>
2021-07-07 17:55:23 -0300

    Add functionality for navigation + an app bar

--- app/src/main/java/com/example/dogs/view/MainActivity.kt
+++ app/src/main/java/com/example/dogs/view/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.dogs.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = (supportFragmentManager.findFragmentById( R.id.fragmentContainerView) as NavHostFragment).findNavController()
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
